03f5c91a6d2a7ac3b06c9bd63de3c56471329571
OlegSkachko <chaegolik@gmail.com>
2022-03-16 19:04:37 +0300

    [new][M] INFRBRON-13 added Icon component and IIcon

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,14 @@ import { PATHS } from './const'
 import './App.css'
 import Invite from './components/Invite/Invite'
 import AuthorizationForm from './components/Authorization/AuthorizationForm'
-import Icon from './components/Icon/Icon'
 
 const App = ({ context }: any): React.ReactElement => (
-  // <Routes>
-  //   <Route
-  //     path={PATHS.ALL.path}
-  //     element={<Combine staticContext={context} {...PATHS.ALL} />}
-  //   />
-  // </Routes>
-  <Icon
-    value='поздравление'
-  />
+  <Routes>
+    <Route
+      path={PATHS.ALL.path}
+      element={<Combine staticContext={context} {...PATHS.ALL} />}
+    />
+  </Routes>
 )
 
 export default App
